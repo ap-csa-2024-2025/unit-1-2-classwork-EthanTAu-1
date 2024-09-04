b24858750fda7f40c11be0fbc817607f473b6d7b
EthanTAu <etau100@student.hbuhsd.edu>
2024-09-04 11:05:44 -0700

    classword

--- problem3.java
+++ problem3.java
@@ -1,1 +1,10 @@
 // Create your class here; don't forget the main method
+public class problem3
+{
+    public static void main(String[] args)
+    {
+        String name = "Ethan Au";
+        String loc = "California";
+        System.out.println("My name is " + name + ". I lvie in " + loc + ".");
+    }
+}
